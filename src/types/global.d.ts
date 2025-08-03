793cf196cdc0ffc32b8c9d430d0157c8af63ca97
余廷涛 <735126858@qq.com>
2025-08-03 20:36:56 +0800

    update

--- src/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-import 'vue';
-
-declare module 'vue' {
-  interface ComponentCustomProperties {
-    $hasPermission: (permission: string) => boolean;
-  }
-}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,10 @@
 /// <reference types="vite/client" />
 
 declare module 'virtual:svg-icons-register';
+
+export {};
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $hasPermission: (permission: string) => boolean;
+  }
+}
