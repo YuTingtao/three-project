902c7c8c21ee4e49e486939bdefc123f5eef72d7
YuTingtao <735126858@qq.com>
2025-09-18 13:24:38 +0800

    update

--- src/views/PanoView.vue
+++ src/views/PanoView.vue
@@ -6,7 +6,8 @@
 import { ref } from 'vue';
 import Three3dView from '@/components/three3dView/index.vue';
 
-const sceneUrl = ref('/file/scene/house.jpg');
+const baseUrl = import.meta.env.BASE_URL === '/' ? '.' : import.meta.env.BASE_URL;
+const sceneUrl = ref(baseUrl + 'file/scene/house.jpg');
 </script>
 
 <style lang="scss" scoped>
